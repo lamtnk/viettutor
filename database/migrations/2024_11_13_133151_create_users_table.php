7c3d9ec2ece496d99cbc521a5934f8aee6c5bc25
huylee47 <huyw300@gmail.com>
2024-11-20 14:37:52 +0700

    Update : User migration

--- database/migrations/2024_11_13_133151_create_users_table.php
+++ database/migrations/2024_11_13_133151_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
