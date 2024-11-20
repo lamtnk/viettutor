605e9afb99ff9ea3ed309129718ae4e2e4b0c6ed
huylee47 <huyw300@gmail.com>
2024-11-20 14:39:17 +0700

    Update : route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\FeedbackController;
 use App\Http\Controllers\Client\ContactController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\ListTutorController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,15 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('admin.dashboard.index');
 // });
+Route::prefix('/register')->group(function () {
+    Route::get('/',[RegisterController::class,'index'])->name('register.index');
+    Route::post('/new', [RegisterController::class,'register'])->name('register.register');
+});
+
+Route::prefix('/login')->group(function () {
+    Route::get('/',[LoginController::class,'index'])->name('login.index');
+    Route::post('/auth', [LoginController::class,'login'])->name('login.login');
+});
 Route::prefix('admin')->group(function () {
     Route::prefix('tutor')->group(function () {
         Route::get('/', [TutorController::class, 'index'])->name('tutor.index');
