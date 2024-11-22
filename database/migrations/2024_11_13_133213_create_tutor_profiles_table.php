3fd91797354ac3e79a785be93d2075172ced6c34
ntl1997 <luandajk4@gmail.com>
2024-11-22 11:06:48 +0700

    Add migrations for tutor profiles, courses, subjects, course days, and shifts

--- database/migrations/2024_11_13_133213_create_tutor_profiles_table.php
+++ database/migrations/2024_11_13_133213_create_tutor_profiles_table.php
@@ -23,7 +23,7 @@ class CreateTutorProfilesTable extends Migration
             $table->string('profile_image')->nullable();
             $table->string('front_of_id')->nullable();
             $table->string('back_of_id')->nullable();
-            $table->int('sex')->nullable();
+            $table->integer('sex')->nullable();
             $table->integer('area')->nullable();
             $table->integer('status')->nullable();
             $table->timestamps();
