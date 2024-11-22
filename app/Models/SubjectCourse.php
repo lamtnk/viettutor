5d0fd8ccacb6f8f865dec0dc7851568247f40573
ntl1997 <luandajk4@gmail.com>
2024-11-22 11:11:53 +0700

    Add CourseDay, CourseShift, Subject, and SubjectCourse models with relationships; update Course model to include new fields and methods

---
+++ app/Models/SubjectCourse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubjectCourse extends Model
+{
+    use HasFactory;
+    protected $table = 'subject_course';
+    protected $fillable = [
+        'subject_id',
+        'course_id'
+    ];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class, 'subject_id');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+}
