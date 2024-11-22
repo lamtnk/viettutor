474b3b1e71c37ffafb2d103beb49b5c787ad1ac5
ntl1997 <luandajk4@gmail.com>
2024-11-22 11:05:45 +0700

    Add routes for FindTutor functionality

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\TutorController;
 use App\Http\Controllers\Admin\FeedbackController;
 use App\Http\Controllers\Client\ContactController;
+use App\Http\Controllers\Client\FindTutorController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\ListTutorController;
 use Illuminate\Support\Facades\Route;
@@ -43,4 +44,10 @@ Route::prefix('list-tutor')->group(function () {
 Route::prefix('contact')->group(function () {
     Route::get('/', [ContactController::class, 'index'])->name('contact.index');
     Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
+});
+
+Route::prefix('find-tutor')->group(function () {
+    Route::get('/index', [FindTutorController::class, 'index'])->name('find-tutor.index');
+    Route::post('/store', [FindTutorController::class, 'store'])->name('find-tutor.store');
+    // Route::get('/add', [GameController::class, 'showAddGame'])->name('admin.game.show_add');
 });
